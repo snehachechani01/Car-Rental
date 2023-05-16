f90a5222cb513cc98f455fa2955906c4092a9519
snehachechani01 <sneha.chechani@sigmainfo.net>
2023-05-16 20:46:01 +0530

    updated

--- reactfrontend/src/component/Booked.js
+++ reactfrontend/src/component/Booked.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom'
 
-
+import './Booked.css';
 
 function Booked() {
   const [data, setData] = useState([]);
